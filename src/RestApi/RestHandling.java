80645b507a5eb1353091ad603bf30046cef412bf
Krzysztof Sychla <ksychla5@gmail.com>
2019-11-10 16:16:57 +0100

    Correct structure

--- src/RestApi/RestHandling.java
+++ src/RestApi/RestHandling.java
@@ -12,34 +12,108 @@ import javax.ws.rs.core.Response;
 public class RestHandling {
 
     @GET
-    @Path("surface/{id}")
-    public Response getSurface(@PathParam("id") Integer id){
+    @Path("{id}/surface")
+    public Response getSurfaceBuilding(@PathParam("id") Integer id){
         JsonParser jsonParser = new JsonParser();
         BuildingComponent building = jsonParser.loadJson(id.toString()+".json");
         return Response.ok(String.valueOf(building.GetSurface())).build();
     }
 
     @GET
-    @Path("cubature/{id}")
-    public Response getCubature(@PathParam("id") Integer id){
+    @Path("{id}/cubature")
+    public Response getCubatureBuilding(@PathParam("id") Integer id){
         JsonParser jsonParser = new JsonParser();
         BuildingComponent building = jsonParser.loadJson(id.toString()+".json");
         return Response.ok(String.valueOf(building.GetCubature())).build();
     }
 
     @GET
-    @Path("heating/{id}")
-    public Response getHeating(@PathParam("id") Integer id){
+    @Path("{id}/heating")
+    public Response getHeatingBuilding(@PathParam("id") Integer id){
         JsonParser jsonParser = new JsonParser();
         BuildingComponent building = jsonParser.loadJson(id.toString()+".json");
         return Response.ok(String.valueOf(building.GetHeating())).build();
     }
 
     @GET
-    @Path("wattage/{id}")
-    public Response getLampWattage(@PathParam("id") Integer id){
+    @Path("{id}/wattage")
+    public Response getLampWattageBuilding(@PathParam("id") Integer id){
         JsonParser jsonParser = new JsonParser();
         BuildingComponent building = jsonParser.loadJson(id.toString()+".json");
         return Response.ok(String.valueOf(building.GetLampWattage())).build();
     }
+
+    @GET
+    @Path("{id}/floor/{floor}/surface")
+    public Response getSurface(@PathParam("id") Integer id, @PathParam("floor") String floor){
+        JsonParser jsonParser = new JsonParser();
+        BuildingComponent building = jsonParser.loadJson(id.toString()+".json");
+//        TODO: Add floor
+//        BuildingComponent floorBuildingComponent = building.(...)
+        return Response.ok(String.valueOf(building.GetSurface())).build();
+    }
+
+    @GET
+    @Path("{id}/floor/{floor}/cubature")
+    public Response getCubature(@PathParam("id") Integer id, @PathParam("floor") String floor){
+        JsonParser jsonParser = new JsonParser();
+        BuildingComponent building = jsonParser.loadJson(id.toString()+".json");
+//        TODO
+        return Response.ok(String.valueOf(building.GetCubature())).build();
+    }
+
+    @GET
+    @Path("{id}/floor/{floor}/heating")
+    public Response getHeating(@PathParam("id") Integer id, @PathParam("floor") String floor){
+        JsonParser jsonParser = new JsonParser();
+        BuildingComponent building = jsonParser.loadJson(id.toString()+".json");
+//        TODO
+        return Response.ok(String.valueOf(building.GetHeating())).build();
+    }
+
+    @GET
+    @Path("{id}/floor/{floor}/wattage")
+    public Response getLampWattage(@PathParam("id") Integer id, @PathParam("floor") String floor){
+        JsonParser jsonParser = new JsonParser();
+        BuildingComponent building = jsonParser.loadJson(id.toString()+".json");
+//        TODO
+        return Response.ok(String.valueOf(building.GetLampWattage())).build();
+    }
+
+    @GET
+    @Path("{id}/floor/{floor}/room/{room}/surface")
+    public Response getSurface(@PathParam("id") Integer id, @PathParam("floor") String floor, @PathParam("room") String room){
+        JsonParser jsonParser = new JsonParser();
+        BuildingComponent building = jsonParser.loadJson(id.toString()+".json");
+//        TODO
+        return Response.ok(String.valueOf(building.GetSurface())).build();
+    }
+
+    @GET
+    @Path("{id}/floor/{floor}/room/{room}/cubature")
+    public Response getCubature(@PathParam("id") Integer id, @PathParam("floor") String floor, @PathParam("room") String room){
+        JsonParser jsonParser = new JsonParser();
+        BuildingComponent building = jsonParser.loadJson(id.toString()+".json");
+//        TODO
+        return Response.ok(String.valueOf(building.GetCubature())).build();
+    }
+
+    @GET
+    @Path("{id}/floor/{floor}/room/{room}/heating")
+    public Response getHeating(@PathParam("id") Integer id, @PathParam("floor") String floor, @PathParam("room") String room){
+        JsonParser jsonParser = new JsonParser();
+        BuildingComponent building = jsonParser.loadJson(id.toString()+".json");
+//        TODO
+        return Response.ok(String.valueOf(building.GetHeating())).build();
+    }
+
+    @GET
+    @Path("{id}/floor/{floor}/room/{room}/wattage")
+    public Response getLampWattage(@PathParam("id") Integer id, @PathParam("floor") String floor, @PathParam("room") String room){
+        JsonParser jsonParser = new JsonParser();
+        BuildingComponent building = jsonParser.loadJson(id.toString()+".json");
+//        TODO
+        return Response.ok(String.valueOf(building.GetLampWattage())).build();
+    }
+
 }
