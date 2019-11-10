d9f83096676adf914ccfc56f7a70d0a7fb762346
Krzysztof Sychla <ksychla5@gmail.com>
2019-11-10 09:46:46 +0100

    Implemented REST for building

--- src/RestApi/RestHandling.java
+++ src/RestApi/RestHandling.java
@@ -1,14 +1,45 @@
 package RestApi;
 
+import Building_Info.BuildingComponent;
+import Utils.JsonParser;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
 @Path("/building")
 public class RestHandling {
 
     @GET
-    public Response getSurface(){
-        return Response.ok("test").build();
+    @Path("surface/{id}")
+    public Response getSurface(@PathParam("id") Integer id){
+        JsonParser jsonParser = new JsonParser();
+        BuildingComponent building = jsonParser.loadJson(id.toString()+".json");
+        return Response.ok(String.valueOf(building.GetSurface())).build();
+    }
+
+    @GET
+    @Path("cubature/{id}")
+    public Response getCubature(@PathParam("id") Integer id){
+        JsonParser jsonParser = new JsonParser();
+        BuildingComponent building = jsonParser.loadJson(id.toString()+".json");
+        return Response.ok(String.valueOf(building.GetCubature())).build();
+    }
+
+    @GET
+    @Path("heating/{id}")
+    public Response getHeating(@PathParam("id") Integer id){
+        JsonParser jsonParser = new JsonParser();
+        BuildingComponent building = jsonParser.loadJson(id.toString()+".json");
+        return Response.ok(String.valueOf(building.GetHeating())).build();
+    }
+
+    @GET
+    @Path("wattage/{id}")
+    public Response getLampWattage(@PathParam("id") Integer id){
+        JsonParser jsonParser = new JsonParser();
+        BuildingComponent building = jsonParser.loadJson(id.toString()+".json");
+        return Response.ok(String.valueOf(building.GetLampWattage())).build();
     }
 }
