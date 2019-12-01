b101d17aa9dc593b683fb41f4d7b2a66b7f26ecc
Dominik Witczak <domionato@gmail.com>
2019-12-02 00:59:09 +0100

    Travis javadoc autogen

---
+++ utility/javadoc-to-gh-pages.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "$TRAVIS_REPO_SLUG" == "RenegadeWizard/Building-Info" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+  echo -e "Generating javadoc...\n"
+  nvm javadoc:javadoc
+
+  echo -e "Publishing javadoc...\n"
+
+  cp -R target/site/apidocs $HOME/javadoc-latest
+
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/ReadyTalk/swt-bling gh-pages > /dev/null
+
+  cd gh-pages
+  git rm -rf ./javadoc
+  cp -Rf $HOME/javadoc-latest ./javadoc
+  git add -f .
+  git commit -m "Latest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+  git push -fq origin gh-pages > /dev/null
+
+  echo -e "Published Javadoc to gh-pages.\n"
