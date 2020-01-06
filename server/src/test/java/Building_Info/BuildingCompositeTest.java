6733b9aa98142072c91deffb706bda7a9342bb59
Krzysztof Sychla <ksychla5@gmail.com>
2020-01-06 11:30:06 +0100

    fix: room and buildingComposite tests

--- server/src/test/java/Building_Info/BuildingCompositeTest.java
+++ server/src/test/java/Building_Info/BuildingCompositeTest.java
@@ -4,54 +4,56 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
 
 public class BuildingCompositeTest {
     BuildingComposite buildingComposite;
 
     @Before
     public void setup(){
-        Room mockObject=mock(Room);
-        when(mockObject.getSurface()).thenReturn(10);
-        when(mockObject.getLampWattage()).thenReturn(3);
-        when(mockObject.getCubature()).thenReturn(25);
-        when(mockObject.getHeating()).thenReturn(0);
+        Room mockObject = mock(Room.class);
+        when(mockObject.GetSurface()).thenReturn((float) 10);
+        when(mockObject.GetLampWattage()).thenReturn(3);
+        when(mockObject.GetCubature()).thenReturn((float) 25);
+        when(mockObject.GetHeating()).thenReturn((float) 0);
         buildingComposite = new BuildingComposite(1,"1stfloor");
-        buildingComposite.addChild(mockObject);
+        buildingComposite.AddChild(mockObject);
     }
 
     @Test
     public void getChildCount() {
-        assertEquals(1,buildingComposite.getChildCount());
+        assertEquals(1,buildingComposite.GetChildCount());
     }
 
     @Test
     public void getSurface() {
-        assertEquals(10, buildingComposite.getSurface());
+        assertEquals(10, buildingComposite.GetSurface(), 0.001);
     }
 
     @Test
     public void getLampWattage() {
-        assertEquals(3, buildingComposite.getLampWattage());
+        assertEquals(3, buildingComposite.GetLampWattage());
     }
 
     @Test
     public void getCubature() {
-        assertEquals(25, buildingComposite.getCubature());
+        assertEquals(25, buildingComposite.GetCubature(), 0.001);
     }
 
     @Test
     public void getHeating() {
-        assertEquals(0, buildingComposite.getHeating());
+        assertEquals(0, buildingComposite.GetHeating(), 0.001);
     }
 
     @Test
     public void lampPerMeter2() {
-        assertEquals(0.3, buildingComposite.lampPerMeter2());
+        assertEquals(0.3, buildingComposite.LampPerMeter2(), 0.001);
     }
 
     @Test
     public void heatPerMeter3() {
-        assertEquals(0,buildingComposite.heatPerMeter3());
+        assertEquals(0,buildingComposite.HeatPerMeter3(), 0.001);
     }
 
 }
--- server/src/test/java/Building_Info/RoomTest.java
+++ server/src/test/java/Building_Info/RoomTest.java
@@ -10,12 +10,12 @@ public class RoomTest {
 
 @Before
 public void setup(){
-    room=new Room(1, "Sleeproom", 10, 3,25);
+    room = new Room(1, "Sleeproom", 10, 3,25);
 }
 
     @Test
     public void getSurface() {
-    assertEquals(10, room.GetSurface());
+    assertEquals(10, room.GetSurface(), 0.001);
     }
 
     @Test
@@ -25,21 +25,21 @@ public void setup(){
 
     @Test
     public void getCubature() {
-    assertEquals(25, room.GetCubature());
+    assertEquals(25, room.GetCubature(), 0.001);
     }
 
     @Test
     public void getHeating() {
-    assertEquals(0, room.GetHeating());
+    assertEquals(0, room.GetHeating(), 0.001);
     }
 
     @Test
     public void lampPerMeter2() {
-    assertEquals(0.3, room.LampPerMeter2());
+    assertEquals(0.3, room.LampPerMeter2(), 0.001);
     }
 
     @Test
     public void heatPerMeter3() {
-    assertEquals(0, room.HeatPerMeter3());
+    assertEquals(0, room.HeatPerMeter3(), 0.001);
     }
 }
